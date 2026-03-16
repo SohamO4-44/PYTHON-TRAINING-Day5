63bb5226db14d534087fe5d53989639709ac1307
Soham Umap <24umaps@rbunagpur.in>
2026-03-16 22:30:13 +0530

    Create fuction.py

---
+++ fuction.py
@@ -0,0 +1,15 @@
+def add():
+    n1 = int(input("Enter the value of n1: "))
+    n2 = int(input("Enter the value of n2: "))
+
+    addition = n1 + n2
+    multiplication = n1 * n2
+    subtraction = n1 - n2
+    division = n1 / n2
+
+    print("Addition =", addition)
+    print("Multiplication =", multiplication)
+    print("Subtraction =", subtraction)
+    print("Division =", division)
+
+add()
